946c8dae0ea121b69f8f1da01ac1922c1fcdfdf5
motopig <zxs0220@gmail.com>
2016-06-06 12:14:05 +0800

    view 容器初始化

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -17,14 +17,6 @@ class BaseController
 
     public function __construct( $Container )
     {
-        $Container['view'] =  function ($c) {
-            $view = new \Slim\Views\Twig(VIEW_BASE_PATH, ['cache' => BASE_PATH .'/cache']);
-            $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
-            $view->addExtension(new \Slim\Views\TwigExtension($c['router'], $basePath));
-
-            return $view;
-        };
-
         $this->ci = $Container;
     }
 
--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,17 @@ use Slim\App;
 
 $app = new App();
 
+$container = $app->getContainer();
+
+//init twig view
+$container['view'] =  function ($c) {
+    $view = new \Slim\Views\Twig(VIEW_BASE_PATH, ['cache' => BASE_PATH .'/cache']);
+    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new \Slim\Views\TwigExtension($c['router'], $basePath));
+
+    return $view;
+};
+
 $app->get('/', '\App\Controllers\DefaultController:land');
 
 $app->get('/hello/{name}', function ($request, $response, $args) {
